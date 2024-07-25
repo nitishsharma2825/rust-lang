6854c09cff398d7d46c4a8cf58a095f4b2469ab9
nitishsharma2825 <sharnit2825@gmail.com>
2024-07-25 20:00:30 +0530

    u

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,23 @@ struct Rectangle {
     h: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.w * self.h
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.w > other.w && self.h > other.h
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            w: size,
+            h: size
+        }
+    }
+}
+
 fn main() {
 
     let rect1 = Rectangle {
@@ -11,11 +28,18 @@ fn main() {
         h: 50
     };
 
-    println!("The area of the rectange is {}.", area(&rect1));
-    println!("rect1 is {rect1:?}");
-    println!("rect1 is {rect1:#?}");
-}
+    let rect2 = Rectangle {
+        w: 10,
+        h: 40
+    };
 
-fn area(w: &Rectangle) -> u32 {
-    w.w*w.h
+    let rect3 = Rectangle {
+        w: 60,
+        h: 45
+    };
+
+    let sq = Rectangle::square(3);
+    
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
